977891421b61c79319d0855f3b48e3720182a572
Jason Tu <jasontu4@gmail.com>
2017-08-21 22:25:26 -0400

    Initial commit

---
+++ lib/pack.ex
@@ -0,0 +1,18 @@
+defmodule Pack do
+  @moduledoc """
+  Documentation for Pack.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Pack.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/pack_test.exs
@@ -0,0 +1,8 @@
+defmodule PackTest do
+  use ExUnit.Case
+  doctest Pack
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
