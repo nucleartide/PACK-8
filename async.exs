f38be93013e2906e6c6e0e9fe2cd14c4bbf22ba5
Jason Tu <jasontu4@gmail.com>
2017-09-13 07:45:00 -0400

    Add HTTP request handling

---
+++ async.exs
@@ -0,0 +1,51 @@
+
+HTTPoison.start()
+
+tasks =
+  for i <- 1..6 do
+    Task.async(fn ->
+      # normal
+      # HTTPoison.get("https://raw.githubusercontent.com/tj/make/master/neutrino.mk", [], hackney: [pool: :default])
+
+      # 404
+      # HTTPoison.get("https://raw.githubusercontent.com/tj/make/master/neutrin", [], hackney: [pool: :default])
+
+      # error
+      # HTTPoison.get("http://localhost:1", [], hackney: [pool: :default])
+
+      # task death
+      # exit(:normal)
+
+      # timeout
+      # Process.sleep(6 * 1000)
+    end)
+  end
+
+tasks_with_results = Task.yield_many(tasks)
+
+results = Enum.map(tasks_with_results, fn {task, res} ->
+  # shutdown the tasks that did not reply nor exit
+  res || Task.shutdown(task, :brutal_kill)
+end)
+
+Enum.reduce(results, nil, fn (result, _acc) ->
+  # TODO: on error, return an error tuple
+  # when the accumulator is an error tuple, return the acc
+
+  case result do
+    {:ok, {:ok, %HTTPoison.Response{status_code: 200, body: body}}} ->
+      IO.puts body
+    {:ok, {:ok, %HTTPoison.Response{status_code: status_code}}} ->
+      IO.puts "request failed: status code #{status_code}"
+    {:ok, {:error, %HTTPoison.Error{reason: reason}}} ->
+      IO.puts "request failed: reason #{reason}"
+    {:exit, reason} -> # task died
+      IO.puts "request failed: task died"
+    nil ->
+      IO.puts "timed out"
+  end
+end)
+
+for {:ok, value} <- results do
+  # IO.inspect(value)
+end
