d4bf6a4c0d0decca4d5d963e9cf15d03e64a018a
Jason Tu <jasontu4@gmail.com>
2017-10-11 02:50:07 -0400

    Refactor

--- resolver/github.ex
+++ resolver/github.ex
@@ -1,70 +1,23 @@
-# defmodule Resolver.GitHub.PathError do
-#   defexception [:message]
-# 
-#   defp msg(path) do
-#     ~s(path "#{path}" is invalid, format is github.com/<user>/<repo>/<file>)
-#   end
-# 
-#   def exception(path) do
-#     %Resolver.GitHub.PathError{message: msg(path)}
-#   end
-# end
-
-# defmodule Resolver.GitHub.Error do
-#   @type t :: PathError | HTTPoison.Error
-# end
-
 defmodule Resolver.GitHub do
+  require Lua
+  require Errors
+
   @behaviour Resolver
-  @typep fetch :: (... -> %HTTPoison.Response{})
   @typep github_url :: {user :: String.t, repo :: String.t, file :: String.t}
 
   @doc ~S"""
-  get fetches the contents for a file on GitHub.
+  Fetch the contents for a file on GitHub.
 
-  `path` should conform to github.com/<user>/<repo>/<file> format.
-
-      iex> get = fn _ ->
-      ...>   {:ok, %HTTPoison.Response{status_code: 200, body: "test file"}}
-      ...> end
-      iex> Resolver.GitHub.get("github.com/nucleartide/PACK-8/file", get)
-      {:ok, "test file"}
-
-      iex> Resolver.GitHub.get("this isn't github wtf")
-      {:error, %Resolver.GitHub.PathError{message: "path \"this isn't github wtf\" is invalid, format is github.com/<user>/<repo>/<file>"}}
-
-      iex> get = fn _ ->
-      ...>   {:ok, %HTTPoison.Response{status_code: 404}}
-      ...> end
-      iex> Resolver.GitHub.get("github.com/nucleartide/PACK-8/file", get)
-      {:error, %HTTPoison.Error{reason: "received 404 for https://raw.githubusercontent.com/nucleartide/PACK-8/master/file.lua"}}
-
+  Note that `path` should conform to github.com/<user>/<repo>/<file>
+  format.
   """
-  @spec get(String.t, fetch) :: {:ok, String.t} | {:error, Error.t}
-  def get(path, fetch \\ &HTTPoison.get/1) do
+  @spec get(path :: String.t) :: {:ok, String.t} | {:error, Exception.t}
+  def get(path) do
     with {:ok, github_url} <- validate(path),
-         {:ok, content}    <- github_url |> url() |> req(fetch),
-         do: {:ok, content}
-  end
-
-  # Validate that `path` conforms to the github.com/<user>/<repo>/<file>
-  # format.
-  # 
-  # If validation succeeds, this function returns a tuple containing the
-  # user, repo, and file.
-  # 
-  # Else, this function returns a Resolver.GitHub.PathError.
-  @spec validate(path :: String.t) :: {:ok, github_url} | {:error, Exception.t}
-  defp validate(path) do
-    parts = path
-      |> Installer.normalize()
-      |> String.split("/")
-
-    case parts do
-      [".", "github", "com", user, repo | file] when length(file) > 0 ->
-        {:ok, {user, repo, Enum.join(file, "/")}}
-      _ ->
-        {:error, Resolver.GitHub.PathError.exception(path)}
+         res = {:ok, _}    <- github_url |> url() |> req() do
+      res
+    else
+      {:error, e} -> Errors.wrap(e, "couldn't fetch github file")
     end
   end
 
@@ -74,22 +27,42 @@ defmodule Resolver.GitHub do
     "https://raw.githubusercontent.com/#{user}/#{repo}/master/#{file}"
   end
 
+  # Validate that `path` conforms to the github.com/<user>/<repo>/<file>
+  # format.
+  @spec validate(path :: String.t) :: {:ok, github_url} | {:error, Exception.t}
+  defp validate(path) do
+    parts = path
+      |> Lua.normalize()
+      |> String.split("/")
+
+    case parts do
+      [".", "github", "com", user, repo | file] when length(file) > 0 ->
+        {:ok, {user, repo, Enum.join(file, "/")}}
+      _ ->
+        {:error, path |> invalid_msg() |> Errors.new()}
+    end
+  end
+
+  @spec invalid_msg(path :: String.t) :: String.t
+  defp invalid_msg(path) do
+    ~s(#{path} is invalid, format is github.com/<user>/<repo>/<file>)
+  end
+
   # req makes an HTTP request to a specified `url`.
   #
   # It will return an HTTPoison.Error for responses without a status
   # code of 200.
-  @spec req(String.t, fetch) :: {:ok, String.t} | {:error, Exception.t}
-  defp req(url, fetcher) do
+  @spec req(url :: String.t) :: {:ok, String.t} | {:error, Exception.t}
+  defp req(url) do
     HTTPoison.start()
 
-    case fetcher.(url) do
+    case HTTPoison.get(url) do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
         {:ok, body}
       {:ok, %HTTPoison.Response{status_code: status_code}} ->
-        reason = "received #{status_code} for #{url}"
-        {:error, %HTTPoison.Error{reason: reason}}
-      err ->
-        err
+        {:error, Errors.new("received #{status_code} for #{url}")}
+      {:error, e} ->
+        {:error, Errors.wrap(e, "http request failed")}
     end
   end
 end
--- sigil/sigil.ex
+++ sigil/sigil.ex
@@ -1,10 +1,9 @@
-
 defmodule Sigil do
   @doc """
   sigil_m is a helper for writing multi-line regexes.
 
-  It will strip away leading and trailing whitespace for
-  each line, and pass it on to the ~r sigil.
+  It will strip away leading and trailing whitespace for each line, and
+  pass it on to the ~r sigil.
   """
   def sigil_m(string, []) do
     ~r/#{String.split(string) |> Enum.join()}/
